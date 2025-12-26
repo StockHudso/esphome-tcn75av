87b32e39bf38ded6c71b80e4ce2f941b32aa9d79
StockHudso <54939616+StockHudso@users.noreply.github.com>
2025-12-26 18:46:44 +0100

    Add TCN75AV sensor header file

---
+++ components/tcn75av/tcn75av_sensor.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
+
+namespace esphome {
+namespace tcn75av {
+
+class TCN75AVSensor : public PollingComponent,
+                      public sensor::Sensor,
+                      public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void update() override;
+};
+
+}  // namespace tcn75av
+}  // namespace esphome
